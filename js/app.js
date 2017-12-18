45faf002a02ae87d13150ac066dbdc442d6f918f
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-12-18 01:45:03 -0500

    modals inicia seción y registrarse

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // La funcionalidad de tu proyecto
 $(document).ready(function() {
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+  $('.button-collapse').sideNav();
+  $('.parallax').parallax();
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+  $('select').material_select();
 });
